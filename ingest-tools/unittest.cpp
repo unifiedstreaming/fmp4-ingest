22cac4c7868c174877c55e0b363a8f3b11abeeb8
rufael <rufael@unfied-streaming.com>
2021-03-09 17:37:29 +0100

    - dry run - removed chunked mode - unit tests - multiple emsg per sample/segment

--- ingest-tools/unittest.cpp
+++ ingest-tools/unittest.cpp
@@ -5,7 +5,7 @@ unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
+TEST_CASE( "MPD Event to Sparse Track", "[event2sparse]" ) {
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
