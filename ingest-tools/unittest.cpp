2eee37961cb3923600addcc66f69e95a5272eb00
rufael <rufael@unfied-streaming.com>
2021-03-10 11:05:47 +0100

    adding unit testing (in progress)

--- ingest-tools/unittest.cpp
+++ ingest-tools/unittest.cpp
@@ -5,9 +5,35 @@ unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "MPD Event to Sparse Track", "[event2sparse]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("vectors can be sized and resized", "[vector]") {
+
+	std::vector<int> v(5);
+
+	REQUIRE(v.size() == 5);
+	REQUIRE(v.capacity() >= 5);
+
+	SECTION("resizing bigger changes size and capacity") {
+		v.resize(10);
+
+		REQUIRE(v.size() == 10);
+		REQUIRE(v.capacity() >= 10);
+	}
+	SECTION("resizing smaller changes size but not capacity") {
+		v.resize(0);
+
+		REQUIRE(v.size() == 0);
+		REQUIRE(v.capacity() >= 5);
+	}
+	SECTION("reserving bigger changes capacity but not size") {
+		v.reserve(10);
+
+		REQUIRE(v.size() == 5);
+		REQUIRE(v.capacity() >= 10);
+	}
+	SECTION("reserving smaller does not change size or capacity") {
+		v.reserve(0);
+
+		REQUIRE(v.size() == 5);
+		REQUIRE(v.capacity() >= 5);
+	}
 }
