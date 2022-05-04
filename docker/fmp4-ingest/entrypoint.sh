e0850929b7d50f040eb357fa3c907a780aa1c5db
jamie <jamie@unified-streaming.com>
2022-05-04 15:46:08 +0200

    test build with new entrypoint script with MODE arg/env

---
+++ docker/fmp4-ingest/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+set -e
+
+if [ -n "$MODE" ]
+    # set env vars to defaults if not already set
+    if [ -z "$ANNOUNCE" ]
+      then
+      export ANNOUNCE=10
+    fi
+
+    # validate required variables are set
+    if [ -z "$PUB_POINT_URI" ]
+      then
+      echo >&2 "Error: PUB_POINT_URI environment variable is required but not set."
+      exit 1
+    fi
+    if [ -z "$GOP_LENGTH_MS" ]
+      then
+      echo >&2 "Error: GOP_LENGTH_MS environment variable is required but not set."
+      exit 1
+    fi
+    if [ -z "$AVAIL_LENGTH_MS" ]
+      then
+      echo >&2 "Error: AVAIL_LENGTH environment variable is required but not set."
+      exit 1
+    fi
+
+    if [ -z "$AVAIL_INTERVAL_MS" ]
+      then
+      echo >&2 "Error: AVAIL_INTERVAL environment variable is required but not set."
+      exit 1
+    fi
+
+    # Compute offset timeing information
+    ms_since_epoch=$(date +%s%3N)
+    multiple_gops_since_epoch=$(expr $ms_since_epoch / $GOP_LENGTH_MS)
+    ism_offset=$(expr $(expr $multiple_gops_since_epoch + 1) \* $GOP_LENGTH_MS)
+
+    # Invoke fmp4ingest using result and variables
+    fmp4ingest -r --ism_offset $ism_offset --ism_use_ms -u $PUB_POINT_URI --announce $ANNOUNCE --avail $AVAIL_INTERVAL_MS $AVAIL_LENGTH_MS
+else
+    exec "$@"
+fi
